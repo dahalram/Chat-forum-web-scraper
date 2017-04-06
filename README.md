a3df168083b87bed06e02cd3c3ecfd0574c6dd18
Ram Hari Dahal <ramhari.dahal1@gmail.com>
2017-04-06 10:50:29 -0400

    user agents added

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Chat-forum-web-scraper
 Web scraper for Jamiiforums.com
+
+Install scrapy
+pip install scrapy_proxies
--- test.py
+++ test.py
@@ -9,6 +9,7 @@ import requests
 # # URL to scrape from
 # web_link = "https://www.jamiiforums.com/threads/teach-individuals-and-not-the-class.1202358/"
 
+# http://willdrevo.com/using-a-proxy-with-a-randomized-user-agent-in-python-requests/
 # page = urllib2.urlopen(web_link)
 # print page
 
@@ -28,7 +29,7 @@ def LoadUserAgents(uafile=USER_AGENTS_FILE):
 # load the user agents, in random order
 user_agents = LoadUserAgents(uafile="user_agents.txt")
 
-proxy = {"http": "http://username:p3ssw0rd@10.10.1.10:3128"}
+proxy = {"http": "http://<username>:<p3ssw0rd>@10.10.1.10:3128"}
 url = 'http://api.openweathermap.org/data/2.5/weather'
 params = {"q" : "London,uk"}
 
